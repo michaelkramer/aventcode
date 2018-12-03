6d822fb05c122419d992b4d2c6a7966d8c15c8a4
Michael Kramer <michaelrkramer@gmail.com>
2018-12-03 08:55:49 -0500

    day 2

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 // @flow
-
 export function startWebServer() {
   // this function exists standalone is case there are Web server startup scripts
 
-  require('./server/web/router'); // eslint-disable-line
+  require("./server/web/router"); // eslint-disable-line
 }
